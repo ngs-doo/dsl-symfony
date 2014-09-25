a81304a2d375f95ea2eca5f258b143828fd0fd59
Viktor Jagusic <viktor.jagusic@gmail.com>
2014-09-25 18:11:42 +0200

    Fixed collections type check

--- src/NGS/Symfony/Form/Type/CollectionType.php
+++ src/NGS/Symfony/Form/Type/CollectionType.php
@@ -1,24 +1,12 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace NGS\Symfony\Form\Type;
 
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\Extension\Core\EventListener\ResizeFormListener;
-use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType as SymfonyCollection;
 
 class CollectionType extends SymfonyCollection
@@ -34,7 +22,7 @@ class CollectionType extends SymfonyCollection
         if ($options['controller'])
             $options['type'] = $options['controller']->getFormType();
 
-        if (isset ($options['type']))
+        if (isset ($options['type']) && $options['type'] instanceof FormTypeInterface)
             $options['prototype_name'] = '__' . $options['type']->getName() . '__';
 
         parent::buildForm ($builder, $options);
